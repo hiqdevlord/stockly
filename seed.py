4c229590eccb665cde77d6421e0dc007cbb7eaf7
hiqdevlord <hiqdevlord@gmail.com>
2013-01-24 16:43:59 +0530

    . adding an url_seeder

--- seed.py
+++ seed.py
@@ -1,9 +1,15 @@
-import urllib2 as urlib
-from BeautifulSoup import BeautifulSoup
+import models
+total_number=3168
+per_page=66
+show_next=0
+base_url="http://in.finance.yahoo.com/q/hp?s=INFY.BO&a=2&b=16&c=2000&d=0&e=22&f=2013&g=d&z=66&y="+str(show_next)
+url_store=models.Url()
+def write_mongo(url,url_store):
+	url_store.urls.append(url)
+while show_next<=total_number:
+	write_mongo((base_url+str(show_next)),url_store)
+	show_next+=per_page
+url_store.save()
 
 
-base_url="http://in.finance.yahoo.com/q/hp?s=INFY.BO"
-html_page=urlib.urlopen(base_url)
-html_document=BeautifulSoup(html_page)
-for url in html_document('a',rel="next"):
-	print base_url+url["href"]
+
