929d17ca56b0207b42d9ec223ccf020a8d03aa6c
hiqdevlord <hiqdevlord@gmail.com>
2013-01-25 16:27:47 +0530

    Hashing the URLs and usingthem as key and minor changes to the Schema

--- crawl.py
+++ crawl.py
@@ -1,12 +1,20 @@
 import models as model
 import urllib2 as urlib
+import pyhash
+
+hasher=pyhash.murmur3_32()
 
 def write_html_dump(url,html_store):
 	html_dump=urlib.urlopen(url).read()
-	html_store.html_dump[url]=html_dump
+	url_hash=hasher(url)
+	html_store.html_dump[str(url_hash)]=html_dump
+
 
 html_store=model.Html_dump()
 urls=model.Url.objects()
+write_html_dump(urls[0].urls[0],html_store)
+
 for url in urls[0].urls:
-	print "Crawling:"+url
 	write_html_dump(url,html_store)
+
+html_store.save()
--- models.py
+++ models.py
@@ -9,6 +9,6 @@ class Index(Document):
 	indexes=ListField(DictField(field=MapField(field=StringField())))
 
 class Html_dump(Document):
-	html_dump=MapField(field=StringField())
+	html_dump=DictField()
 
 
