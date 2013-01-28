b57e5c15a9ad5309f5a56c6228a18c56ea7f34ae
hiqdevlord <hiqdevlord@gmail.com>
2013-01-28 22:41:46 +0530

    Crawler Final Cut

--- crawl.py
+++ crawl.py
@@ -1,20 +1,12 @@
 import models as model
 import urllib2 as urlib
-import pyhash
 
-hasher=pyhash.murmur3_32()
+def write_html(url):
+	
+	html_store=model.Html_dump()
+	html_store.url=url
+	print "Dumping:"+url.url
+	html_store.html_dump=urlib.urlopen(url.url).read()
+	html_store.save()
 
-def write_html_dump(url,html_store):
-	html_dump=urlib.urlopen(url).read()
-	url_hash=hasher(url)
-	html_store.html_dump[str(url_hash)]=html_dump
-
-
-html_store=model.Html_dump()
-urls=model.Url.objects()
-write_html_dump(urls[0].urls[0],html_store)
-
-for url in urls[0].urls:
-	write_html_dump(url,html_store)
-
-html_store.save()
+for url in model.Urls.objects: write_html(url)
