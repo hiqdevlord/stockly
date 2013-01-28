0b4aea9a069b79d94a2cc5013a24da33d7d733d9
hiqdevlord <hiqdevlord@gmail.com>
2013-01-28 22:41:08 +0530

    Reflecting Schema changes on the seeder

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ from mongoengine import *
 
 connect('stockly')
 
-class Url(Document):
-	urls=URLField()
+class Urls(Document):
+	url=URLField()
 
 class Index(Document):
 	indexes=ListField(DictField(field=MapField(field=StringField())))
 
 class Html_dump(Document):
-	url =URLField()
-	html_dump=StringField()
+	url = ReferenceField(Urls)
+	html_dump=DynamicField()
 
 
--- seed.py
+++ seed.py
@@ -4,7 +4,7 @@ per_page=66
 show_next=0
 base_url="http://in.finance.yahoo.com/q/hp?s=INFY.BO&a=2&b=16&c=2000&d=0&e=22&f=2013&g=d&z=66&y="+str(show_next)
 def write_mongo(url):
-	url_store=models.Url()
+	url_store=models.Urls()
 	url_store.url=url
 	url_store.save()
 while show_next<=total_number:
