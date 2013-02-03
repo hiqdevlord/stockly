42a77381d44173744cb0a5bd24e6ae8fdc925557
hiqdevlord <hiqdevlord@gmail.com>
2013-02-03 17:18:44 +0530

    modified:   parse.py Finising the parser

--- parse.py
+++ parse.py
@@ -1,9 +1,13 @@
 import models as model
 from BeautifulSoup import BeautifulSoup
-from datetime import datetime 
+from dateutil.parser import parse
 import re
+import logging 
+
+logging.basicConfig(filename='parser.log')
 def to_date(date_string):
-	return datetime.strptime(date_string,"%d %b %Y")
+	
+	return parse(date_string)
 
 def cast(item):
 	try:
@@ -15,19 +19,20 @@ def cast(item):
 	except:
 		return to_date(item)
 def extract_attributes(html_store):
-	page=BeautifulSoup(html_store.html_dump)
-	table=page.find('table','yfnc_datamodoutline1')
-	price_rows=table.findAllNext('tr')
-	for price_row in price_rows[2:-4]:
-		index=model.Index()
-		try:
-			indices=[index.date,index.open_price,index.day_high,index.day_low,index.close_price,index.volume,index.adj_close]=[cast(re.sub(',','',field.string)) for field in price_row.findChildren('td','yfnc_tabledata1')]
-			html_store.indexes=indices
-			html_store.save()
-		except:
-			raise 
-#for html_dump in model.Html_dump.objects: extract_attributes(html_dump)
-extract_attributes(model.html_data.objects[48])
+	try:
+		page=BeautifulSoup(html_store.html_dump)
+		table=page.find('table','yfnc_datamodoutline1')
+		price_rows=table.findAllNext('tr')
+		for price_row in price_rows[2:-4]:
+			index=model.Index()
+			try:
+				indices=[index.date,index.open_price,index.day_high,index.day_low,index.close_price,index.volume,index.adj_close]=[cast(re.sub(',','',field.string)) for field in price_row.findChildren('td','yfnc_tabledata1')]
+				html_store.indexes.append(index)
+			except:
+				logging.warning('Skipping %s',field.string)
+		html_store.save()
+		logging.info('%s is parsed and dumped',html_store.url)
+	except:
+		logging.exception("Document is empty or doesn't have valid HMTL")
+for html_store in model.html_data.objects: extract_attributes(html_store)
 
-
-#to-do:Writing everything in unicode is a bad idea,do proper dynamic type conversion
