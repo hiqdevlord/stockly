a2ada1023c696a4d64d289de1ecc286dc69f5c4d
hiqdevlord <hiqdevlord@gmail.com>
2013-03-15 10:06:41 +0530

    Sentiment Analysis WIP

--- sentiment/sentiment_parse.py
+++ sentiment/sentiment_parse.py
@@ -1,6 +1,15 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
 from pattern.en import sentiment
-for article in open('articles.txt','r').readlines():
-       	print sentiment(BeautifulSoup(urllib2.urlopen(article).read()).find('body'))
+import re
+def sentiment_values(html_document):
+	sentiments=[sentiment(elem) for elem in html_document(text=re.compile(r'(I|i)nfosys'))] 
+	return len(sentiments),reduce(lambda x,y: (x[0]+y[0],x[1]+y[1]),sentiments)
+for article in open('articles.txt','r').readlines()[0:2]:
+       	html_document=BeautifulSoup(urllib2.urlopen(article).read())
+	length,sentiment=sentiment_values(html_document)
+	#print map(lambda x:x/length,sentiment)
+	print length
+	
+
 	
